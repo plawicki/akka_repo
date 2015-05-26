02000018c1735cefc7419c3322d02be79c27a3ff
Patryk <patlaw96@gmail.com>
2015-05-26 19:32:32 +0200

    adding cluster

---
+++ akka/src/main/java/com/patrykl/akka/MyActor.java
@@ -0,0 +1,61 @@
+package com.patrykl.akka;
+
+import akka.actor.UntypedActor;
+import akka.cluster.Cluster;
+import akka.cluster.ClusterEvent.CurrentClusterState;
+import akka.cluster.ClusterEvent.MemberEvent;
+import akka.cluster.ClusterEvent.MemberRemoved;
+import akka.cluster.ClusterEvent.MemberUp;
+import akka.cluster.ClusterEvent.UnreachableMember;
+import akka.dispatch.BoundedMessageQueueSemantics;
+import akka.dispatch.RequiresMessageQueue;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+
+public class MyActor extends UntypedActor implements RequiresMessageQueue<BoundedMessageQueueSemantics> {
+
+	LoggingAdapter log = Logging.getLogger(getContext().system(), this);
+	Cluster cluster = Cluster.get(getContext().system());
+
+	// subscribe to cluster changes
+	@Override
+	public void preStart() {
+		// #subscribe
+		cluster.subscribe(getSelf(), MemberEvent.class, UnreachableMember.class);
+		// #subscribe
+	}
+
+	// re-subscribe when restart
+	@Override
+	public void postStop() {
+		cluster.unsubscribe(getSelf());
+	}
+
+	@Override
+	public void onReceive(Object message) {
+		if (message instanceof CurrentClusterState) {
+			CurrentClusterState state = (CurrentClusterState) message;
+			log.info("Current members: {}", state.members());
+
+		} else if (message instanceof MemberUp) {
+			MemberUp mUp = (MemberUp) message;
+			log.info("Member is Up: {}", mUp.member());
+
+		} else if (message instanceof UnreachableMember) {
+			UnreachableMember mUnreachable = (UnreachableMember) message;
+			log.info("Member detected as unreachable: {}", mUnreachable.member());
+
+		} else if (message instanceof MemberRemoved) {
+			MemberRemoved mRemoved = (MemberRemoved) message;
+			log.info("Member is Removed: {}", mRemoved.member());
+
+		} else if (message instanceof MemberEvent) {
+			// ignore
+
+		} else {
+			unhandled(message);
+		}
+
+	}
+
+}
